4a75182178d5f08c14635e09e8edfd66c014af2e
Birk <birk.kensen@gmail.com>
2022-04-28 20:13:21 +0200

    removed api since they are no longer needed

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,8 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 // import clientPromise from '../../database/mongodb';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	// const collection = (await clientPromise).db('portfolio').collection('projects');
-	// const projects = await collection.find({}).toArray();
-	// return res.status(200).json(projects);
-}
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {}
