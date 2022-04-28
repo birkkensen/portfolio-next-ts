747157ede2d61453f4938f390bd601169c7c4d6b
Birk <birk.kensen@gmail.com>
2022-04-28 20:30:57 +0200

    added sharp package for image optimization in production

--- database/mongodb.ts
+++ database/mongodb.ts
@@ -1,7 +1,6 @@
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 dotenv.config();
-
 if (!process.env.MONGODB_URI) {
 	throw new Error('Please add your Mongo URI to .env.local');
 }
