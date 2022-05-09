5c440e30c3a3db6b5f75e69e2eab8787f60eaa52
Birk <birk.kensen@gmail.com>
2022-05-09 23:15:13 +0200

    longer animation duration on page transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,20 +6,6 @@ import '@fontsource/montserrat/500.css';
 import '@fontsource/montserrat/700.css';
 import type { AppProps } from 'next/app';
 function MyApp({ Component, pageProps, router }: AppProps) {
-	const animation = {
-		initial: {
-			opacity: 0,
-		},
-		animate: {
-			opacity: 1,
-		},
-		exit: {
-			opacity: 0,
-		},
-		transition: {
-			duration: 0.7,
-		},
-	};
 	return (
 		<LazyMotion features={domAnimation}>
 			<AnimatePresence exitBeforeEnter onExitComplete={() => window.scrollTo(0, 0)}>
@@ -28,7 +14,7 @@ function MyApp({ Component, pageProps, router }: AppProps) {
 					initial={{ opacity: 0 }}
 					animate={{ opacity: 1 }}
 					exit={{ opacity: 0 }}
-					transition={{ duration: 0.3 }}
+					transition={{ duration: 0.7 }}
 				>
 					<Layout>
 						<Component {...pageProps} />
