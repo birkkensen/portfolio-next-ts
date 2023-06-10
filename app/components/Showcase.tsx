6fb2b64f0f38a5f483efab08550783e5013dbe46
Birk Kensén <birk.kensen@gloot.com>
2023-06-10 14:13:11 +0200

    remove unnecessary optional check

--- app/components/Showcase.tsx
+++ app/components/Showcase.tsx
@@ -16,14 +16,14 @@ export function Showcase({ project }: PageProps) {
         <AspectRatio ratio={16 / 9}>
           <Image
             style={{
-              objectFit: project.objectFit ?? "cover",
+              objectFit: project.objectFit,
             }}
-            className="h-[384px] w-[512px] transition-all duration-500 group-hover:scale-105"
             blurDataURL={`data:image/jpeg;base64,${project.coverImage.base64}`}
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            className="transition-all duration-500 group-hover:scale-105"
             src={project.coverImage.url}
-            alt={project.name}
             placeholder="blur"
+            alt={project.name}
             fill
           />
           <div className="absolute bottom-0 flex h-2/5 w-full flex-col justify-end gap-3 rounded-b-lg bg-gradient-to-b from-transparent to-black">
