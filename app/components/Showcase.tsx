90e7a7a4ffe9005d10956ed0b697260dc73f42ad
Birk Kensén <birk.kensen@gloot.com>
2023-06-10 14:01:58 +0200

    trying set w & h for correct aspect ratio

--- app/components/Showcase.tsx
+++ app/components/Showcase.tsx
@@ -18,12 +18,12 @@ export function Showcase({ project }: PageProps) {
             style={{
               objectFit: project.objectFit ?? "cover",
             }}
+            className="h-[384px] w-[512px] transition-all duration-500 group-hover:scale-105"
             blurDataURL={`data:image/jpeg;base64,${project.coverImage.base64}`}
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            className="transition-all duration-500 group-hover:scale-105"
             src={project.coverImage.url}
-            placeholder="blur"
             alt={project.name}
+            placeholder="blur"
             fill
           />
           <div className="absolute bottom-0 flex h-2/5 w-full flex-col justify-end gap-3 rounded-b-lg bg-gradient-to-b from-transparent to-black">
