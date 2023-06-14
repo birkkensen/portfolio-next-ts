34f9153cbcc2ad2313f122fa99ec1e0e9cd280a8
Birk Kensén <birk.kensen@gloot.com>
2023-06-14 07:38:33 +0200

    format all

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-'use client'
+"use client";
 
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 
 import { Showcase } from "./components/Showcase";
 import Intro from "./components/Intro";
@@ -8,19 +8,16 @@ import { projects } from "./(data)";
 
 export default function Home() {
   return (
-    <motion.div
-    initial={{opacity: 0}} animate={{ opacity: 1 }}
-    >
+    <motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }}>
       <Intro />
       <section className="grid grid-cols-1 gap-10 md:grid-cols-2">
         <h2 className="col-[1_/_-1] mb-6 mt-5 text-5xl font-bold text-charcoal">
           My learning process <br />
           so far
         </h2>
-        {projects
-          .map((project) => (
-            <Showcase project={project} key={project.id} />
-          ))}
+        {projects.map((project) => (
+          <Showcase project={project} key={project.id} />
+        ))}
       </section>
     </motion.div>
   );
